50e6e1d3651ab15eadc6ef4204c2793f6761810a
Jschles1 <jschles1@ramapo.edu>
2017-10-01 23:56:11 -0400

    Added routes for decks, deleted test directory

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :decks, only: [:index, :show, :create, :update, :destroy]
+  end
 end
