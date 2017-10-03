b97e6c9ef0b5daa5b1a6dc1d1bb9e5d23b345d6b
Jschles1 <jschles1@ramapo.edu>
2017-10-03 00:52:03 -0400

    In index.js, imported semantic-ui-css, Provider, createStore, applyMiddleWare, & thunk. Imported BrowserRouter in App.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'semantic-ui-css/semantic.css'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
