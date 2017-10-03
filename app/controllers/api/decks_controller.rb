5451e7bdc6bd739f6ff6a544fb92147104c7d272
Jschles1 <jschles1@ramapo.edu>
2017-10-03 00:16:50 -0400

    Added show, create, update & destroy actions

--- app/controllers/api/decks_controller.rb
+++ app/controllers/api/decks_controller.rb
@@ -6,6 +6,32 @@ class API::DecksController < ApplicationController
     render json: @decks, status: 200
   end
 
+  def show
+    render json: @deck, status: 200
+  end
+
+  def create
+    @deck = Deck.new(deck_params)
+    if @deck.save
+      render json: @deck, status: 200
+    else
+      render json: { errors: @deck.errors }, status: 422
+    end
+  end
+
+  def update
+    @deck.update(deck_params)
+    if @deck.save
+      render json: @deck, status: 200
+    else
+      render json: { errors: @deck.errors }, status: 422
+    end
+  end
+
+  def destroy
+    @deck.destroy
+  end
+
   private
 
   def deck_params
