d2f2bd7fd20cd2b28225673f5541c820d9e9564e
Jschles1 <jschles1@ramapo.edu>
2017-10-03 00:27:10 -0400

    Added create action & card_params private method

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -5,7 +5,20 @@ class API::CardsController < ApplicationController
     render json: @deck.cards, status: 200
   end
 
+  def create
+    @card = @deck.cards.create(card_params)
+    if @card.save
+      render json: @card, status: 201
+    else
+      render json: { errors: @card.errors }, status: 422
+    end
+  end
+
   private
+
+  def card_params
+    params.require(:card).permit(:question, :answer, :deck_id)
+  end
 
   def set_deck
     @deck = Deck.find(params[:id])
