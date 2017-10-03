48b68206184e5a98aab4ecb62d0f5d9a2d03cf4a
Jschles1 <jschles1@ramapo.edu>
2017-10-03 00:18:53 -0400

    Added index action & set_deck private method

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -1,2 +1,13 @@
 class API::CardsController < ApplicationController
+  before_action :set_deck
+
+  def index
+    render json: @deck.cards, status: 200
+  end
+
+  private
+
+  def set_deck
+    @deck = Deck.find(params[:id])
+  end
 end
