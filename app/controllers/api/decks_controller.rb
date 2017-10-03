002d42dbbd17e070408c10c1b74a9a78135d4285
Jschles1 <jschles1@ramapo.edu>
2017-10-03 00:13:44 -0400

    Added index action, deck_params & set_deck private methods

--- app/controllers/api/decks_controller.rb
+++ app/controllers/api/decks_controller.rb
@@ -1,2 +1,18 @@
 class API::DecksController < ApplicationController
+  before_action :set_deck, only: [:show, :update, :destroy]
+
+  def index
+    @decks = Deck.all
+    render json: @decks, status: 200
+  end
+
+  private
+
+  def deck_params
+    params.require(:deck).permit(:name)
+  end
+
+  def set_deck
+    @deck = Deck.find(params[:id])
+  end
 end
