f528d372fe3bfb60af535002756c9416fc9d2ccc
Jschles1 <jschles1@ramapo.edu>
2017-10-03 23:13:58 -0400

    imported rootReducer & executed createStore()

--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,15 @@ import 'semantic-ui-css/semantic.css'
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)  
+)
 
 ReactDOM.render(
   <Provider>
