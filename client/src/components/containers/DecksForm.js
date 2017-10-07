1a0b0d1320b87e339bbe602387de2ff763e32fac
Jschles1 <jschles1@ramapo.edu>
2017-10-07 01:09:31 -0400

    Added value, onChange, name attributes to inputs. Created handleOnChange event handler

--- client/src/components/containers/DecksForm.js
+++ client/src/components/containers/DecksForm.js
@@ -12,13 +12,20 @@ class DecksForm extends React.Component {
     }
   }
 
+  handleOnChange = (event) => {
+    const { name, value } = event.target; 
+    this.setState({
+      [name]: value
+    })
+  }
+
   render() {
     return(
       <Segment>
         <Form>
           <Form.Group widths="equal">
-            <Form.Input label="Deck Name" placeholder="Deck Name"/>
-            <Form.Input label="Subject" placeholder="Subject"/>
+            <Form.Input value={this.state.name} onChange={this.handleOnChange} name="name" label="Deck Name" placeholder="Deck Name"/>
+            <Form.Input value={this.state.subject} onChange={this.handleOnChange} name="subject" label="Subject" placeholder="Subject"/>
           </Form.Group>
           <Button basic color="black" content="Create Deck" type="submit" icon="plus" labelPosition="right"/>
         </Form>
