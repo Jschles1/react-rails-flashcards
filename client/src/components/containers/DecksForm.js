5a8a3c446d1bd0ac57871a262a059d5b5ed55c7c
Jschles1 <jschles1@ramapo.edu>
2017-10-07 01:28:35 -0400

    Imported actions & created mapDispatchToProps & handleOnSubmit event handler

--- client/src/components/containers/DecksForm.js
+++ client/src/components/containers/DecksForm.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import * as actions from '../../actions/deckActions';
+import { bindActionCreators } from 'redux';
 import { Segment, Form, Input, Button } from 'semantic-ui-react';
 
 class DecksForm extends React.Component {
@@ -19,10 +21,14 @@ class DecksForm extends React.Component {
     })
   }
 
+  handleOnSubmit = (event) => {
+    this.props.actions.submitNewDeck(this.state);
+  }
+
   render() {
     return(
       <Segment>
-        <Form>
+        <Form onSubmit={this.handleOnSubmit}>
           <Form.Group widths="equal">
             <Form.Input value={this.state.name} onChange={this.handleOnChange} name="name" label="Deck Name" placeholder="Deck Name"/>
             <Form.Input value={this.state.subject} onChange={this.handleOnChange} name="subject" label="Subject" placeholder="Subject"/>
@@ -38,4 +44,8 @@ const mapStateToProps = (state) => {
   return { decks: state.decks };
 }
 
-export default connect(mapStateToProps, {})(DecksForm);
+const mapDispatchToProps = (dispatch) => {
+  return { actions: bindActionCreators(actions, dispatch) }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DecksForm);
