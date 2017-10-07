cd2a7ac8318caa072b1b306b14c3dccd77e434a4
Jschles1 <jschles1@ramapo.edu>
2017-10-06 23:52:02 -0400

    Added table header structure

--- client/src/components/DecksList.js
+++ client/src/components/DecksList.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Table, Button } from 'semantic-ui-react';
 
 const DecksList = (props) => {
   const { decks } = this.props;
+
+  const renderDecks = "";
+  
   return(
-    <div></div>
+    <div>
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Deck Name:</Table.HeaderCell>
+            <Table.HeaderCell>Subject:</Table.HeaderCell>
+            <Table.HeaderCell>Number Of Sets:</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {(decks) ? renderDecks : null}
+        </Table.Body>
+      </Table>
+    </div>
   );
 }
 
