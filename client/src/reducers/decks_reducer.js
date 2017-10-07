15016d8d5ffb58a20b53a07c5d8bfc23a0ed4b91
Jschles1 <jschles1@ramapo.edu>
2017-10-07 01:51:32 -0400

    Added case for FETCH_DECKS

--- client/src/reducers/decks_reducer.js
+++ client/src/reducers/decks_reducer.js
@@ -1,5 +1,7 @@
 export default function decksReducer(state = [], action) {
   switch(action.type) {
+    case 'FETCH_DECKS':
+      return action.payload;
     case 'ADD_DECK':
       return state.concat(action.payload);
     default:
