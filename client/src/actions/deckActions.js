2c15539ddce77ce2e7f7164bfc916203ed626d95
Jschles1 <jschles1@ramapo.edu>
2017-10-07 01:16:22 -0400

    Created & exported submitNewDeck action creator, created handleErrors callback

---
+++ client/src/actions/deckActions.js
@@ -0,0 +1,30 @@
+import fetch from 'isomorphic-fetch';
+
+export function submitNewDeck(data, history) {
+  return function(dispatch) {
+    return fetch(`/api/decks`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(handleErrors)
+      .then(resp => {
+        return resp.json();
+      })
+      .then(deck => {
+        dispatch({type: 'ADD_DECK', payload: deck});
+      })
+    .catch((error) => {
+      dispatch({type: 'ERROR'})
+    })
+  }
+}
+
+function handleErrors(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+}
