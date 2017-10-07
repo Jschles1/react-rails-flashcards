7f1d05c4d6d466266d0c3e8a8436d30694b76415
Jschles1 <jschles1@ramapo.edu>
2017-10-07 01:39:57 -0400

    Added mapDispatchToProps & componentDidMount that calls fetchDecks

--- client/src/components/DecksList.js
+++ client/src/components/DecksList.js
@@ -4,8 +4,18 @@ import { Table, Button } from 'semantic-ui-react';
 const DecksList = (props) => {
   const { decks } = this.props;
 
-  const renderDecks = "";
-  
+  const renderDecks = decks.map(deck => {
+    return(
+      <Table.Row key={deck.id}>
+        <Table.Cell>{deck.name}</Table.Cell>
+        <Table.Cell>{deck.subject}</Table.Cell>
+        <Table.Cell>
+          <Button fluid content="View Cards" basic color="blue" icon="right arrow" labelPosition="right"/>
+        </Table.Cell>
+      </Table.Row>
+    );
+  })
+
   return(
     <div>
       <Table celled>
@@ -13,7 +23,7 @@ const DecksList = (props) => {
           <Table.Row>
             <Table.HeaderCell>Deck Name:</Table.HeaderCell>
             <Table.HeaderCell>Subject:</Table.HeaderCell>
-            <Table.HeaderCell>Number Of Sets:</Table.HeaderCell>
+            <Table.HeaderCell>Options:</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
         <Table.Body>
