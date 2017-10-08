97e424f8fbe8fe2807e4510813707efac15bffa7
Jschles1 <jschles1@ramapo.edu>
2017-10-07 21:46:38 -0400

    Added cases for FETCH_EXERCISES & ADD_EXERCISE

--- client/src/reducers/cards_reducer.js
+++ client/src/reducers/cards_reducer.js
@@ -1,5 +1,9 @@
 export default function decksReducer(state = [], action) {
   switch(action.type) {
+    case 'FETCH_EXERCISES':
+      return action.payload;
+    case 'ADD_EXERCISE':
+      return state.concat(action.payload);
     default:
       return state;
   }
