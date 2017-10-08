5a7f18677df170925725d314a1e3e223eb27a32d
Jschles1 <jschles1@ramapo.edu>
2017-10-07 21:38:17 -0400

    Added fetchCards action creator

--- client/src/actions/cardActions.js
+++ client/src/actions/cardActions.js
@@ -1,5 +1,14 @@
 import fetch from 'isomorphic-fetch';
 
+export function fetchCards(deckId) {
+  return function(dispatch) {
+    dispatch({type: 'LOADING_EXERCISES'})
+    return fetch(`/api/cards/${deckId}/cards`)
+      .then(resp => resp.json())
+      .then(cards => dispatch({type: 'FETCH_CARDS', payload: cards}))
+  }
+}
+
 export function submitNewCard(data, deckId) {
   return function(dispatch) {
     return fetch(`/api/decks/${deckId}/cards`, {
