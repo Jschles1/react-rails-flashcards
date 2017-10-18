aaedc213b45c5096e40fc124fa951288ae0ee45a
Jschles1 <jschles1@ramapo.edu>
2017-10-17 20:55:32 -0400

    Changed CardsList to functional stateless component

--- client/src/components/CardsList.js
+++ client/src/components/CardsList.js
@@ -1,37 +1,36 @@
 import React from 'react';
 import { Table, Button } from 'semantic-ui-react';
 
-class CardsList extends React.PureComponent {
-  render() {
-    const renderCards = this.props.cards.map(card => {
-      return(
-        <Table.Row key={card.id}>
-          <Table.Cell>{card.question}</Table.Cell>
-          <Table.Cell>{card.answer}</Table.Cell>
-          <Table.Cell>
-            <Button fluid id={card.id} onClick={this.props.deleteCard} content="Delete Card" basic color="red"/>
-          </Table.Cell>
-        </Table.Row>
-      );
-    })
-
+const CardsList = (props) => {
+  const { cards, deleteCard } = props;
+  const renderCards = cards.map(card => {
     return(
-      <div>
-        <Table celled>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>Question:</Table.HeaderCell>
-              <Table.HeaderCell>Answer:</Table.HeaderCell>
-              <Table.HeaderCell>Options:</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {(this.props.cards) ? renderCards : null}
-          </Table.Body>
-        </Table>
-      </div>
+      <Table.Row key={card.id}>
+        <Table.Cell>{card.question}</Table.Cell>
+        <Table.Cell>{card.answer}</Table.Cell>
+        <Table.Cell>
+          <Button fluid id={card.id} onClick={deleteCard} content="Delete Card" basic color="red"/>
+        </Table.Cell>
+      </Table.Row>
     );
-  }
+  })
+
+  return(
+    <div>
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Question:</Table.HeaderCell>
+            <Table.HeaderCell>Answer:</Table.HeaderCell>
+            <Table.HeaderCell>Options:</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {(cards) ? renderCards : null}
+        </Table.Body>
+      </Table>
+    </div>
+  );
 }
 
 export default CardsList;
