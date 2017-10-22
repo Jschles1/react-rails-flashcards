d3ea938971a8c8b2255dc3e7810f7c5b03e37193
Jschles1 <jschles1@ramapo.edu>
2017-10-22 02:18:16 -0400

    Added componentDidMount() to QuizPage

--- client/src/components/containers/QuizPage.js
+++ client/src/components/containers/QuizPage.js
@@ -4,6 +4,11 @@ import * as actions from '../../actions/deckActions';
 import { bindActionCreators } from 'redux';
 
 class QuizPage extends React.Component {
+
+  componentDidMount() {
+    this.props.actions.fetchDecks();
+  }
+
   render() {
     return(
       <div></div>
