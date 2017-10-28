e274089ce8c2c7d6e92298bdad14cb52f3105c46
Jschles1 <jschles1@ramapo.edu>
2017-10-27 22:03:23 -0400

    Added format for table headers to ChooseDeckForQuiz

--- client/src/components/ChooseDeckForQuiz.js
+++ client/src/components/ChooseDeckForQuiz.js
@@ -10,13 +10,24 @@ const ChooseDeckForQuiz = (props) => {
       <Table.Cell>{deck.name}</Table.Cell>
       <Table.Cell>{deck.subject}</Table.Cell>
       <Table.Cell></Table.Cell>
-      <Table.Cell></Table.Cell>
     </Table.Row>
     );
   })
   return(
     <div>
       <h1>Choose a Deck:</h1>
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Deck Name:</Table.HeaderCell>
+            <Table.HeaderCell>Subject:</Table.HeaderCell>
+            <Table.HeaderCell>Options:</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {(decks) ? renderDecks : null}
+        </Table.Body>
+      </Table>
     </div>
   );
 }
