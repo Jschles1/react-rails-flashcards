21fc60d32d7d5393f4ac0b68a55efe76a8ece20a
Jschles1 <jschles1@ramapo.edu>
2017-10-28 00:11:09 -0400

    Added 2nd version of card using Segment

--- client/src/components/containers/Quiz.js
+++ client/src/components/containers/Quiz.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions/cardActions';
 import { bindActionCreators } from 'redux';
-import { Segment, Button } from 'semantic-ui-react';
+import { Grid, Segment, Button } from 'semantic-ui-react';
 
 class Quiz extends React.Component {
   constructor(props) {
@@ -18,9 +18,16 @@ class Quiz extends React.Component {
   render() {
     return(
       <div>
-        <div className="card">Hello</div>
+        {/* <div className="card">Hello</div>
         <br></br>
-        <Button content="Show Answer" basic color="black"/>
+        <Button size="big" content="Show Answer" basic color="black"/> */}
+        <Grid textAlign="center" style={{ height: "100%" }} verticalAlign="middle">
+          <Grid.Column style={{ maxWidth: 600 }}>
+            <Segment stacked style={{ padding: 50 }}>
+              <p>Hello</p>
+            </Segment>
+          </Grid.Column>
+        </Grid>
       </div>
     );
   }
