92f2df2352215e312f0162e2df06913def432b16
Jschles1 <jschles1@ramapo.edu>
2017-10-27 22:44:05 -0400

    Defined initial state, implemented Segment

--- client/src/components/containers/Quiz.js
+++ client/src/components/containers/Quiz.js
@@ -2,11 +2,24 @@ import React from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions/cardActions';
 import { bindActionCreators } from 'redux';
+import { Segment, Button } from 'semantic-ui-react';
 
 class Quiz extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      question: true,
+      answer: false
+    }
+  }
+
   render() {
     return(
-      <div></div>
+      <div>
+        <Segment stacked textAlign="center">
+        </Segment>
+      </div>
     );
   }
 }
