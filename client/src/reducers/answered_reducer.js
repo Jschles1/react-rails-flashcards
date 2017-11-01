ccb557bfad519b71a58611f74cf062caf00b1bbe
Jschles1 <jschles1@ramapo.edu>
2017-10-31 22:07:16 -0400

    Created & exported answeredReducer into rootReducer

---
+++ client/src/reducers/answered_reducer.js
@@ -0,0 +1,6 @@
+export default function answeredReducer(state = [], action) {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -4,12 +4,14 @@ import cardsReducer from './cards_reducer';
 import errorsReducer from './errors_reducer';
 import loadingDecksReducer from './loading_decks_reducer';
 import loadingCardsReducer from './loading_cards_reducer';
+import answeredReducer from './answered_reducer';
 
 const rootReducer = combineReducers({ 
   decks: decksReducer,
   cards: cardsReducer,
   loadingDecks: loadingDecksReducer,
   loadingCards: loadingCardsReducer,
+  answered: answeredReducer,
   errors: errorsReducer
 });
 
