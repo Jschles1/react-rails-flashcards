f865f7e1f700e911955904c100bfa70b847f669e
Jschles1 <jschles1@ramapo.edu>
2017-10-31 23:56:18 -0400

    Reformatted Show answer button, added handleShowAnswer event handler, added onClick attribute to Show answer button that calls handleShowAnswer & disabled attribute

--- client/src/components/containers/Quiz.js
+++ client/src/components/containers/Quiz.js
@@ -14,6 +14,13 @@ class Quiz extends React.Component {
     }
   }
 
+  handleShowAnswer = () => {
+    this.setState({
+      showAnswer: true,
+      disabled: false
+    })
+  }
+
   render() {
     let card = this.props.cards[Math.floor(Math.random() * this.props.cards.length)]
     return(
@@ -24,7 +31,14 @@ class Quiz extends React.Component {
             <Segment stacked style={{ padding: 50 }}>
               {(this.state.showAnswer) ? card.answer : card.question}
             </Segment>
-            <Button style={{ marginBottom: 20, marginTop: 20 }} size="big" circular content="Show Answer"/>
+            <Button 
+              style={{ marginBottom: 20, marginTop: 20 }} 
+              onClick={this.handleShowAnswer} 
+              disabled={(this.state.showAnswer) ? true : false}
+              size="big" 
+              circular 
+              content="Show Answer"
+            />
             <br></br>
             <Button style={{ margin: 10 }} size="massive" disabled={this.state.disabled} color="red" circular icon="remove"/>
             <Button style={{ margin: 10 }} size="massive" disabled={this.state.disabled} color="green" circular icon="check"/>
