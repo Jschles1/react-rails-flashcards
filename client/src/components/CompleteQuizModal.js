8c062ba403f72554981117f48d015a8538a5ca50
Jschles1 <jschles1@ramapo.edu>
2017-11-05 19:21:46 -0500

    Created & exported CompleteQuizModal into Quiz

---
+++ client/src/components/CompleteQuizModal.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button, Header, Icon, Modal } from 'semantic-ui-react';
+
+const CompleteQuizModal = (props) => {
+  return(
+    <Modal></Modal>
+  );
+}
+
+export default CompleteQuizModal;
