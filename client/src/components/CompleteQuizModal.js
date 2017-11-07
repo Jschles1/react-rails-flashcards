bbc486a1a6af8b97f5426d06e2364f951100eacc
Jschles1 <jschles1@ramapo.edu>
2017-11-07 18:26:47 -0500

    Completed structure of modal

--- client/src/components/CompleteQuizModal.js
+++ client/src/components/CompleteQuizModal.js
@@ -2,8 +2,24 @@ import React from 'react';
 import { Button, Header, Icon, Modal } from 'semantic-ui-react';
 
 const CompleteQuizModal = (props) => {
+  const { modalOpen, finalScore, completeQuiz } = props;
   return(
-    <Modal></Modal>
+    <Modal
+      open={modalOpen}
+      closeOnEscape={false}
+      closeOnDimmerClick={false}
+      closeOnRootNodeClick={false}
+    >
+      <Modal.Header>
+        Quiz Complete!
+      </Modal.Header>
+      <Modal.Content>
+        <p>Score: {finalScore}</p>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button basic color="black" labelPosition="right"  content="Continue" />
+      </Modal.Actions>
+    </Modal>
   );
 }
 
