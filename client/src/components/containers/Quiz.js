f4a57d8848a1821ecb5d5425b2a5bdde2de97d1d
Jschles1 <jschles1@ramapo.edu>
2017-11-10 01:02:12 -0500

    Removed handleRightWrong(), handleShowAnswer(), & state

--- client/src/components/containers/Quiz.js
+++ client/src/components/containers/Quiz.js
@@ -9,66 +9,12 @@ class Quiz extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      showAnswer: false,
-      disabled: true,
-      count: 0,
-      correct: 0,
-      incorrect: 0
-    }
-  }
-
-  handleShowAnswer = () => {
-    this.setState({
-      showAnswer: true,
-      disabled: false
-    })
-  }
-
-  handleRightWrong = (event) => {
-    let noNext;
-    let incrementCount;
-    const { name, id } = event.target;
-    const { cards, actions, deckId, triggerModal } = this.props;
-    let { count, correct, incorrect } = this.state;
-
-    if (cards.length > 1) {
-      noNext = false;
-      incrementCount = ++count;
-    } else {
-      noNext = true;
-      incrementCount = count;
-    }
-
-    if (name === "correct") {
-      this.setState({
-        [name]: ++correct,
-        disabled: true,
-        showAnswer: noNext,
-        count: incrementCount
-      })
-    } else {
-      this.setState({
-        [name]: ++incorrect,
-        disabled: true,
-        showAnswer: noNext,
-        count: incrementCount
-      })
-    }
-
-    if (cards.length > 1) {
-      actions.removeAnsweredCard(id);
-    } else {
-      const score = correct / (count + 1);
-      actions.submitScore(deckId, score, () => {
-        triggerModal(score);
-      });
-    }
+    this.state = {}
   }
 
   render() {
+    const { handleShowAnswer, handleRightWrong, showAnswer, disabled, cards, count } = this.props;
     const card = this.props.cards[0]
-    const { showAnswer, disabled, count } = this.state;
     return(
       <div>
         <Header style={{ marginBottom: 15 }}>Question {count + 1}</Header>
@@ -79,15 +25,15 @@ class Quiz extends React.Component {
             </Segment>
             <Button 
               style={{ marginBottom: 20, marginTop: 20 }} 
-              onClick={this.handleShowAnswer} 
+              onClick={handleShowAnswer} 
               disabled={(showAnswer) ? true : false}
               size="big" 
               circular 
               content="Show Answer"
             />
             <br></br>
-            <Button id={card.id} name="correct" style={{ margin: 10 }} onClick={this.handleRightWrong} size="massive" disabled={disabled} color="green" circular content="Correct"/>
-            <Button id={card.id} name="incorrect" style={{ margin: 10 }} onClick={this.handleRightWrong} size="massive" disabled={disabled} color="red" circular content="Incorrect"/>
+            <Button id={card.id} name="correct" style={{ margin: 10 }} onClick={handleRightWrong} size="massive" disabled={disabled} color="green" circular content="Correct"/>
+            <Button id={card.id} name="incorrect" style={{ margin: 10 }} onClick={handleRightWrong} size="massive" disabled={disabled} color="red" circular content="Incorrect"/>
           </Grid.Column>
         </Grid>
       </div>
