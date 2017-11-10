fac9c7315de8b966aff118a4bd792f3da1641add
Jschles1 <jschles1@ramapo.edu>
2017-11-10 00:18:52 -0500

    Added initial state to answeredReducer, added cases for incrementing each attribute as well as resetting each attribute

--- client/src/reducers/answered_reducer.js
+++ client/src/reducers/answered_reducer.js
@@ -1,5 +1,13 @@
-export default function answeredReducer(state = [], action) {
+export default function answeredReducer(state = { count: 0, correct: 0, incorrect: 0}, action) {
   switch(action.type) {
+    case 'INCREMENT_COUNT':
+      return { ...state, count: state.count++ };
+    case 'INCREMENT_CORRECT':
+      return { ...state, count: state.correct++ };
+    case 'INCREMENT_INCORRECT':
+      return { ...state, count: state.incorrect++ };
+    case 'RESET_COUNTS':
+      return { count: 0, correct: 0, incorrect: 0};
     default:
       return state;
   }
