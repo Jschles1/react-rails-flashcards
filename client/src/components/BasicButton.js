80b899c75d6beb80cdfd9b126c3703c2f8066217
Jschles1 <jschles1@ramapo.edu>
2017-11-25 20:38:26 -0500

    Created BasicButton component

---
+++ client/src/components/BasicButton.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react';
+
+class BasicButton extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+      <Button>
+
+      </Button>
+    );
+  }
+}
+
+export default BasicButton;
