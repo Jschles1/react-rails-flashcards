8a838a2d333b0f561bc3a8ba43e9e3577548b7ee
Jschles1 <jschles1@ramapo.edu>
2017-11-26 01:33:01 -0500

    Imported BasicButton into Navbar & replaced previous My Decks button w/ BasicButton

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Menu, Button } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import BasicButton from './BasicButton';
 
 const Navbar = () => {
   return(
     <Menu>
       <Menu.Item header>Flashcards</Menu.Item>
       <Menu.Item bordered>
-        <Button basic color="black" as={Link} to={`/`}>My Decks</Button>
+        {/* <Button basic color="black" as={Link} to={`/`}>My Decks</Button> */}
+        <BasicButton 
+          isFluid={false}
+          color="black"
+          link={Link}
+          url={`/`}
+          textContent="My Decks"
+        />
       </Menu.Item>
       <Menu.Item>
        <Button basic color="green" as={Link} to={`/quiz`}>Quiz Yourself</Button>
