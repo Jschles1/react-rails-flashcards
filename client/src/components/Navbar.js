affc6205a3162d5edbc78421068618fc0c148744
Jschles1 <jschles1@ramapo.edu>
2017-12-22 17:06:02 -0500

    Removed Button import, changed Navbar to use implicit rendering

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,35 +1,33 @@
 import React from 'react';
-import { Menu, Button } from 'semantic-ui-react';
+import { Menu } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import BasicButton from './BasicButton';
 
-const Navbar = () => {
-  return(
-    <Menu>
-      <Menu.Item header>Flashcards</Menu.Item>
-      <Menu.Item bordered>
-        <BasicButton 
-          isFluid={false}
-          color="black"
-          link={Link}
-          url={`/`}
-          textContent="My Decks"
-        />
-      </Menu.Item>
-      <Menu.Item>
-       <BasicButton 
-          isFluid={false}
-          color="green"
-          link={Link}
-          url={`/quiz`}
-          textContent="Quiz Yourself"
-        />
-      </Menu.Item>
-      <Menu.Item position="right">
-        &copy; John Schlesinger
-      </Menu.Item>
-    </Menu>
-  );
-}
+const Navbar = () => 
+  <Menu>
+    <Menu.Item header>Flashcards</Menu.Item>
+    <Menu.Item bordered>
+      <BasicButton 
+        isFluid={false}
+        color="black"
+        link={Link}
+        url={`/`}
+        textContent="My Decks"
+      />
+    </Menu.Item>
+    <Menu.Item>
+      <BasicButton 
+        isFluid={false}
+        color="green"
+        link={Link}
+        url={`/quiz`}
+        textContent="Quiz Yourself"
+      />
+    </Menu.Item>
+    <Menu.Item position="right">
+      &copy; John Schlesinger
+    </Menu.Item>
+  </Menu>
+
 
 export default Navbar;
