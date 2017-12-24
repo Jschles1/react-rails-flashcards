15ee37676d408bd99271a13f5c514eb098175434
Jschles1 <jschles1@ramapo.edu>
2017-12-24 01:00:26 -0500

    Deleted loadingDecksReducer & loadingCardsReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,16 +2,12 @@ import { combineReducers } from 'redux';
 import decksReducer from './decks_reducer';
 import cardsReducer from './cards_reducer';
 import errorsReducer from './errors_reducer';
-import loadingDecksReducer from './loading_decks_reducer';
-import loadingCardsReducer from './loading_cards_reducer';
 import answeredReducer from './answered_reducer';
 import loadingReducer from './loading_reducer';
 
 const rootReducer = combineReducers({ 
   decks: decksReducer,
   cards: cardsReducer,
-  // loadingDecks: loadingDecksReducer,
-  // loadingCards: loadingCardsReducer,
   loading: loadingReducer,
   answered: answeredReducer,
   errors: errorsReducer
